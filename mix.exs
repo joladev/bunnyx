c1798a2fdaf06d93724d0e271a97211ffb0c1f27
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2026-05-18 10:51:58 -0600

    Normalize CI and loosen Elixir version requirement

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Bunnyx.MixProject do
     [
       app: :bunnyx,
       version: @version,
-      elixir: "~> 1.19",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       warnings_as_errors: true,
